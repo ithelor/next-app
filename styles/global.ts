22b7749a26dfa6e852cf18daf70a9f7f0923ed10
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-07-29 21:03:51 +1000

    ci: replace css with ts

---
+++ styles/global.ts
@@ -0,0 +1,21 @@
+import { css } from '@emotion/react'
+
+export default css`
+  html {
+    font-size: 14px;
+  }
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    border: 0;
+  }
+`
---
+++ styles/theme.ts
@@ -0,0 +1,37 @@
+export const baseTheme = {
+  colors: {
+    primary: '#5DAAFF',
+    accent: '#2876CC',
+    light: '#FFFFFF',
+
+    background: '#E5E5E5',
+    text: '#606F7A'
+  },
+
+  shadows: {
+    normal: '0px 4px 40px 0px #2E50571F'
+  },
+
+  media: {
+    small: '(max-width: 540px)',
+    medium: '(max-width: 720px)',
+    large: '(max-width: 960px)'
+  },
+
+  sizes: {
+    buttons: {
+      normal: {
+        padding: '14px 22px',
+        borderRadius: 3
+      },
+      large: {
+        padding: '25px 39px',
+        borderRadius: 3
+      },
+      wide: {
+        padding: '19px 35px',
+        borderRadius: 5
+      }
+    }
+  }
+}
