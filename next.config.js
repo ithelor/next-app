8a074ca9f738424d3f4a905fe924a6feaf7ac9ee
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-08-03 23:58:57 +1000

    chore: update next config to resize svgs

--- next.config.js
+++ next.config.js
@@ -6,8 +6,23 @@ module.exports = {
 
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: { removeViewBox: false }
+              }
+            }
+          ]
+        },
+        titleProp: true
+      },
+      test: /\.svg$/
     })
 
     return config
