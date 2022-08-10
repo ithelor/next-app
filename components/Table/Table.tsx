66f5ba114b7967ae625f72b17198303c5626286a
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-08-11 01:40:46 +1000

    ci: add controls & data handling, fix keys

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
-import { SelectionEntry } from 'components'
+import { ButtonIcon, SelectionEntry } from 'components'
+
+import { useProductsStore } from 'lib/RootStoreContext'
 
 import { furniture, IProduct } from 'stores/ProductsStore'
 
+import CrossIcon from 'public/cross.svg'
+
 import * as S from './styles'
+
+interface ITable {
+  data: IProduct[]
+}
 
 export const head: { [key: string]: string } = {
   quantity: 'Кол-во',
@@ -14,35 +22,52 @@ export const head: { [key: string]: string } = {
   price: 'Стоимость единицы'
 }
 
-interface ITable {
-  data: IProduct[]
-}
+const addSpaces = (number: number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
 
-export const Table = (props: ITable) => (
-  <S.Table>
-    <thead>
-      <tr>
-        {Object.keys(props.data[0]).map((key: string, index) => (
-          <td key={key}>{head[key]}</td>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {props.data.map((item) => (
-        <tr key={item.name}>
-          {Object.values(item).map((property: string, index) =>
-            index === 0 ? (
-              <td>
-                <SelectionEntry variant="selected">
-                  {furniture.find((item) => item.title === property)!}
-                </SelectionEntry>
-              </td>
-            ) : (
-              <td key={property}>{property || '-'}</td>
-            )
-          )}
+const getUniqueKey = (pre?: string) => `${pre}. ${new Date().getTime()}`
+
+export const Table = (props: ITable) => {
+  const productsStore = useProductsStore()
+
+  return (
+    <S.Table>
+      <thead>
+        <tr>
+          {Object.keys(props.data[0]).map((key: string, index) => (
+            <td key={key}>{head[key]}</td>
+          ))}
+          <td>Удалить</td>
         </tr>
-      ))}
-    </tbody>
-  </S.Table>
-)
+      </thead>
+      <tbody>
+        {props.data.map((item, index) => (
+          <tr key={getUniqueKey(item.name)}>
+            {Object.values(item).map((property: string, index) =>
+              index === 0 ? (
+                <td key={getUniqueKey(`${item.name}'s ${Object.keys(item)[index]}`)}>
+                  <SelectionEntry variant="selected">
+                    {furniture.find((item) => item.title === property)!}
+                  </SelectionEntry>
+                </td>
+              ) : (
+                <td key={getUniqueKey(`${item.name}'s ${Object.keys(item)[index]}`)}>
+                  {(property && addSpaces(Number(property))) || '-'}
+                  {property && index === Object.values(item).length - 1 ? ' руб.' : ''}
+                </td>
+              )
+            )}
+            <td>
+              <ButtonIcon
+                key={getUniqueKey(`${item.name}'s delete button`)}
+                ariaLabel="Remove"
+                onClick={() => productsStore.products.splice(index)}
+              >
+                <CrossIcon />
+              </ButtonIcon>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </S.Table>
+  )
+}
