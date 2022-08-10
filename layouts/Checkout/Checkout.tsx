c5e7f90a6df399ed5844e5ee7e5bd931b28d4134
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-08-11 01:43:14 +1000

    ci: add controls & data handling

--- layouts/Checkout/Checkout.tsx
+++ layouts/Checkout/Checkout.tsx
@@ -1,17 +1,62 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { Observer } from 'mobx-react'
 
-import { Table } from 'components'
+import { ButtonPrimary, Loader, Table } from 'components'
 
 import { useProductsStore } from 'lib/RootStoreContext'
+
+import PlusThickIcon from 'public/plusThick.svg'
 
 import * as S from './styles'
 
 export const Checkout = () => {
+  const router = useRouter()
   const productsStore = useProductsStore()
+
+  const [isLoading, setIsLoading] = React.useState(false)
+
+  const calculate = () => {
+    setIsLoading(true)
+
+    setTimeout(() => setIsLoading(false), 3000)
+  }
 
   return (
     <S.Checkout>
-      <Table data={productsStore.products} />
+      {isLoading ? (
+        <S.Center>
+          <Loader />
+        </S.Center>
+      ) : (
+        <Observer>
+          {() => (
+            <>
+              {productsStore.products.length > 0 ? (
+                <>
+                  <h1>Добавленная мебель ({productsStore.products.length})</h1>
+                  <Table data={productsStore.products} />
+                </>
+              ) : (
+                <h1>Мебель не добавлена</h1>
+              )}
+
+              <S.Controls>
+                <ButtonPrimary ariaLabel="Add" onClick={() => router.push('/selection')}>
+                  Добавить
+                  <PlusThickIcon />
+                </ButtonPrimary>
+
+                {productsStore.products.length > 0 && (
+                  <ButtonPrimary ariaLabel="Calculate" onClick={calculate}>
+                    Рассчитать
+                  </ButtonPrimary>
+                )}
+              </S.Controls>
+            </>
+          )}
+        </Observer>
+      )}
     </S.Checkout>
   )
 }
--- layouts/Checkout/styles.ts
+++ layouts/Checkout/styles.ts
@@ -4,5 +4,34 @@ import { baseTheme } from 'styles/theme'
 
 export const Checkout = styled.main`
   display: flex;
+  flex-direction: column;
+  gap: 2.7rem;
+
+  padding: 0 ${baseTheme.sizes.header.padding.horizontal};
+
+  h1 {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 36px;
+    font-weight: 400;
+    letter-spacing: -1px;
+
+    color: ${baseTheme.colors.text};
+  }
+`
+
+export const Controls = styled.span`
+  position: absolute;
+  right: ${baseTheme.sizes.header.padding.horizontal};
+  bottom: 4rem;
+
+  display: flex;
+  gap: 1rem;
+`
+
+export const Center = styled.div`
+  display: flex;
   justify-content: center;
+  align-items: center;
+
+  height: calc(100vh - ${baseTheme.sizes.header.height});
 `
