e0b603a4f0c486c3c09e05cfcb161d6a0c7b92ac
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-08-11 23:00:13 +1000

    ci: update layout

--- layouts/Checkout/Checkout.tsx
+++ layouts/Checkout/Checkout.tsx
@@ -2,28 +2,70 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import { Observer } from 'mobx-react'
 
-import { ButtonPrimary, Loader, Table } from 'components'
+import { ButtonOutlineWide, ButtonPrimary, ButtonPrimaryLarge, Loader, Table } from 'components'
 
 import { useProductsStore } from 'lib/RootStoreContext'
+import { useHeaderExtras } from 'lib/HeaderExtrasContext'
+
+import { addSpaces } from 'utils'
 
 import PlusThickIcon from 'public/plusThick.svg'
+import ArrowRightIcon from 'public/arrowRight.svg'
 
 import * as S from './styles'
+
+export interface IControls {
+  variant?: 'default' | 'absolute'
+  children?: React.ReactNode
+}
 
 export const Checkout = () => {
   const router = useRouter()
   const productsStore = useProductsStore()
 
+  const headerExtras = useHeaderExtras()
+  const [isFinal, setIsFinal] = React.useState(false)
+
   const [isLoading, setIsLoading] = React.useState(false)
+
+  const deliveryCost = Math.floor((Math.random() + 5) * 10000)
+  const customsCost = Math.floor((Math.random() + 5) * 1000)
+  const totalCost = deliveryCost + customsCost
 
   const calculate = () => {
     setIsLoading(true)
 
-    setTimeout(() => setIsLoading(false), 3000)
+    headerExtras?.setState({ ...headerExtras.state, isProductsTotalActive: true })
+
+    setTimeout(() => {
+      setIsLoading(false)
+      setIsFinal(true)
+
+      headerExtras?.setState({ isParamsActive: false, isProductsTotalActive: true })
+    }, 2000)
   }
 
+  const Controls = (props: IControls) => (
+    <S.Controls variant={props.variant}>
+      {props.children ?? (
+        <>
+          <ButtonPrimary ariaLabel="Add" onClick={() => router.push('/selection')}>
+            Добавить
+            <PlusThickIcon />
+          </ButtonPrimary>
+
+          {productsStore.products.length > 0 && (
+            <ButtonPrimary ariaLabel="Calculate" onClick={calculate}>
+              Рассчитать
+            </ButtonPrimary>
+          )}
+        </>
+      )}
+    </S.Controls>
+  )
+
   return (
-    <S.Checkout>
+    <S.Checkout isFinal={isFinal}>
       {isLoading ? (
         <S.Center>
           <Loader />
@@ -32,27 +74,50 @@ export const Checkout = () => {
         <Observer>
           {() => (
             <>
-              {productsStore.products.length > 0 ? (
+              {isFinal ? (
                 <>
-                  <h1>Добавленная мебель ({productsStore.products.length})</h1>
-                  <Table data={productsStore.products} />
+                  <Table data={productsStore.products} withTotal />
+                  <Controls />
+                  <S.Total>
+                    <div className="labels">
+                      <h3>Стоимость доставки:</h3>
+                      <h3>Таможенные платежи:</h3>
+                      <h2>Итого:</h2>
+                    </div>
+
+                    <div className="costs">
+                      <span>{addSpaces(deliveryCost)} руб.</span>
+                      <span>{addSpaces(customsCost)} руб.</span>
+                      <span>{addSpaces(totalCost)} руб.</span>
+                    </div>
+                  </S.Total>
+
+                  <Controls>
+                    <ButtonOutlineWide ariaLabel="Save" rounder>
+                      Сохранить расчёт
+                      <ArrowRightIcon />
+                    </ButtonOutlineWide>
+                    <ButtonPrimaryLarge ariaLabel="Contact" rounder>
+                      Связаться по доставке
+                    </ButtonPrimaryLarge>
+                  </Controls>
                 </>
               ) : (
-                <h1>Мебель не добавлена</h1>
+                <>
+                  {productsStore.products.length > 0 ? (
+                    <>
+                      <h1>Добавленная мебель ({productsStore.products.length})</h1>
+                      <S.Scrollable>
+                        <Table data={productsStore.products} sticky />
+                      </S.Scrollable>
+                    </>
+                  ) : (
+                    <h1>Мебель не добавлена</h1>
+                  )}
+
+                  <Controls variant="absolute" />
+                </>
               )}
-
-              <S.Controls>
-                <ButtonPrimary ariaLabel="Add" onClick={() => router.push('/selection')}>
-                  Добавить
-                  <PlusThickIcon />
-                </ButtonPrimary>
-
-                {productsStore.products.length > 0 && (
-                  <ButtonPrimary ariaLabel="Calculate" onClick={calculate}>
-                    Рассчитать
-                  </ButtonPrimary>
-                )}
-              </S.Controls>
             </>
           )}
         </Observer>
--- layouts/Checkout/styles.ts
+++ layouts/Checkout/styles.ts
@@ -2,12 +2,19 @@ import styled from '@emotion/styled'
 
 import { baseTheme } from 'styles/theme'
 
-export const Checkout = styled.main`
+import { IControls } from './Checkout'
+
+interface ICheckoutStyled {
+  isFinal: boolean
+}
+
+export const Checkout = styled.main<ICheckoutStyled>`
   display: flex;
   flex-direction: column;
   gap: 2.7rem;
 
   padding: 0 ${baseTheme.sizes.header.padding.horizontal};
+  margin-bottom: ${(props) => props.isFinal && '3.4rem'};
 
   h1 {
     font-family: 'Open Sans', sans-serif;
@@ -19,19 +26,101 @@ export const Checkout = styled.main`
   }
 `
 
-export const Controls = styled.span`
-  position: absolute;
-  right: ${baseTheme.sizes.header.padding.horizontal};
-  bottom: 4rem;
-
-  display: flex;
-  gap: 1rem;
-`
-
 export const Center = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
   height: calc(100vh - ${baseTheme.sizes.header.height});
+`
+
+export const Scrollable = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  max-height: calc(100vh - ${baseTheme.sizes.header.height} - 87px);
+  overflow-y: auto;
+  padding-right: 5px;
+
+  &::-webkit-scrollbar-track {
+    background-color: ${baseTheme.colors.scroll.background};
+  }
+
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 50px;
+    background-color: ${baseTheme.colors.scroll.accent};
+  }
+`
+
+interface IControlsStyled {
+  variant?: IControls['variant']
+}
+
+export const Controls = styled.span<IControlsStyled>`
+  position: ${(props) => props.variant === 'absolute' && 'absolute'};
+  right: ${baseTheme.sizes.header.padding.horizontal};
+  bottom: 4rem;
+
+  display: flex;
+  gap: 1rem;
+
+  margin-left: auto;
+`
+
+export const Total = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  letter-spacing: 0.2px;
+
+  display: flex;
+  gap: 3rem;
+  margin-left: auto;
+
+  color: ${baseTheme.colors.text};
+
+  .labels,
+  .costs {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  .labels {
+    h3,
+    h2 {
+      text-align: right;
+    }
+
+    h3 + h2 {
+      margin-top: 1rem;
+    }
+
+    h3 {
+      font-size: 23px;
+      font-weight: 400;
+    }
+
+    h2 {
+      font-size: 31px;
+      font-weight: 600;
+    }
+  }
+
+  .costs {
+    text-align: right;
+    font-size: 23px;
+    font-weight: 400;
+
+    span {
+      :last-of-type {
+        font-size: 31px;
+        font-weight: 600;
+
+        margin-top: 1rem;
+      }
+    }
+  }
 `
